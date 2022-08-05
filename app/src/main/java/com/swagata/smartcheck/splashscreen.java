a60cbb14e8a2d93ed6c107455d6f08700230a547
SwagataRoy09 <swagataroy.dgp@gmail.com>
2022-08-05 21:17:23 +0530

    Initial commit

---
+++ app/src/main/java/com/swagata/smartcheck/splashscreen.java
@@ -0,0 +1,31 @@
+package com.swagata.smartcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Handler;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        ImageView icon;
+        icon = findViewById(androidx.core.R.id.icon);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        }, 5000);
+    }
+}
